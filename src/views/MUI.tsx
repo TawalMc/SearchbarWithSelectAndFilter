7af99957fdbb8b1b3edb3d23197f3e5658feb915
TawalMc <developtawaliou@gmail.com>
2022-10-29 07:51:28 +0100

    feat: add how the  SearchbarWithSelectAndFilter can be used to get selected options

--- src/views/MUI.tsx
+++ src/views/MUI.tsx
@@ -1,9 +1,12 @@
 import { DummyData, DummyDataType } from '../dummy';
 import SearchbarWithSelectAndFilter from '../component/SearchbarWithSelectAndFilter/SearchbarWithSelectAndFilter';
 import Cards from '../component/Card/Cards';
+import { useState } from 'react';
 
 const MUI = () => {
   const [selectedOptions, setSelectedOptions] = useState<DummyDataType[]>([]);
+
+  console.log(selectedOptions)
 
   return (
     <div>
@@ -14,6 +17,8 @@ const MUI = () => {
         autoCompleteProps={{
           options: DummyData,
           renderInput: () => <></>,
+          value: selectedOptions,
+          onChange: (_event, value) => setSelectedOptions(value),
         }}
       />
       <br />
