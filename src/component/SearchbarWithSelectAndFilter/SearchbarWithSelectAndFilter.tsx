49b37ec91923055ceeadc0bb686032a0eda20f5d
TawalMc <developtawaliou@gmail.com>
2022-12-17 14:16:40 +0100

    add uncategorised  to dropdown list values

--- src/component/SearchbarWithSelectAndFilter/SearchbarWithSelectAndFilter.tsx
+++ src/component/SearchbarWithSelectAndFilter/SearchbarWithSelectAndFilter.tsx
@@ -1,59 +1,79 @@
-import React, {useMemo, useState, useEffect, forwardRef, ForwardedRef} from "react";
+import React, { useMemo, useState, useEffect, forwardRef, ForwardedRef } from "react";
 import {
     DEFAULT_SELECT_VALUE,
     SearchbarSelectCheckboxProps,
+    UNCATEGORISED_SELECT_VALUE,
 } from "./types";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, {SelectChangeEvent} from "@mui/material/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
- 
-const icon = <CheckBoxOutlineBlankIcon fontSize="small"/>;
-const checkedIcon = <CheckBoxIcon fontSize="small"/>;
-const SearchbarWithSelectAndFilterInner = <T, >({
-                                               placeholder,
-                                               searchData = [],
-                                               groupBy,
-                                               toDisplay,
-                                               searchCheckboxProps,
-                                               autoCompleteProps,
-                                               divProps,
-                                               selectProps,
-                                               firstFormControlProps,
-                                               secondFormControlProps,
-                                           }: SearchbarSelectCheckboxProps<T>,
-                                           ref: ForwardedRef<HTMLDivElement>,
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+const SearchbarWithSelectAndFilterInner = <T,>({
+    placeholder,
+    searchData = [],
+    groupBy,
+    toDisplay,
+    withAll = true,
+    allLabel = "All",
+    uncategorised = true,
+    uncategorisedLabel = "Uncategorised",
+    searchCheckboxProps,
+    autoCompleteProps,
+    divProps,
+    selectProps,
+    firstFormControlProps,
+    secondFormControlProps,
+}: SearchbarSelectCheckboxProps<T>,
+    ref: ForwardedRef<HTMLDivElement>,
 ) => {
     /**
      * Hooks
      */
     const [selectedItem, setSelectedItem] = useState(DEFAULT_SELECT_VALUE);
 
-    const [searchList, setSearchList] = useState(new Map<string, T[]>())
+    const [searchList, setSearchList] = useState(new Map<ReturnType<typeof groupBy>, T[]>())
 
     /**
      * Functions
      */
 
-        // will set the selected dropdown's item
+    // will set the selected dropdown's item
     const handleSelectedItemChange = (event: SelectChangeEvent) => {
-            setSelectedItem(event.target.value);
-        };
+        let targetValue = event.target.value
+        /* if (unCategorised) {
+            targetValue = targetValue == undefined || targetValue == null ? UNCATEGORISED_SELECT_VALUE : targetValue;
+        } */
+        setSelectedItem(targetValue)
+    }
 
     // get the list of items for drowpdown
     let itemsGroup = useMemo(() => {
-        let itemsAsSet = new Set(searchData.map(groupBy));
-        return Array.from(itemsAsSet).filter((v) => v !== undefined && v !== null);
+        let itemsAsSet = new Set();
+        searchData.forEach(data => {
+            let option = groupBy(data) ?? undefined;
+            if (uncategorised) {
+                option = option == undefined ? UNCATEGORISED_SELECT_VALUE : option
+                itemsAsSet.add(option)
+            } else if (!uncategorised && option != undefined) {
+                itemsAsSet.add(option)
+            }
+        })
+        return Array.from(itemsAsSet)
     }, [searchData, groupBy]);
+
+    console.log(itemsGroup, 77777777)
 
     // update list of options provided to autocomplete
     useEffect(() => {
         const updateSearchList = new Map(searchList)
-        if(!updateSearchList.has(selectedItem)) {
+        if (!updateSearchList.has(selectedItem)) {
             if (selectedItem == DEFAULT_SELECT_VALUE) {
                 updateSearchList.set(DEFAULT_SELECT_VALUE, searchData)
             } else {
@@ -68,22 +88,24 @@ const SearchbarWithSelectAndFilterInner = <T, >({
 
 
     return (
-        <div ref={ref} style={{display: "flex", alignItems: "center", width: "500px"}}  {...divProps} >
-            <FormControl sx={{minWidth: "150px", marginRight: 1}} {...firstFormControlProps} >
+        <div ref={ref} style={{ display: "flex", alignItems: "center", width: "500px" }}  {...divProps} >
+            <FormControl sx={{ minWidth: "150px", marginRight: 1 }} {...firstFormControlProps} >
                 <Select
                     displayEmpty
                     size="small"
-                    inputProps={{"aria-label": "Without label"}}
+                    inputProps={{ "aria-label": "Without label" }}
                     {...selectProps}
                     value={selectedItem}
                     onChange={handleSelectedItemChange}
                 >
                     <MenuItem value={DEFAULT_SELECT_VALUE}>
-                        Tout
+                        {allLabel ?? "Tout"}
                     </MenuItem>
                     {
-                        itemsGroup.map(item => <MenuItem key={item}
-                                                         value={item}>{item}</MenuItem>)
+                        itemsGroup.map(item =>
+                            <MenuItem key={`${item}`} value={item}>
+                                {`${item == UNCATEGORISED_SELECT_VALUE ? uncategorisedLabel : item}`}
+                            </MenuItem>)
                     }
                 </Select>
             </FormControl>
@@ -101,22 +123,22 @@ const SearchbarWithSelectAndFilterInner = <T, >({
                         },
                     }}
                     options={searchList.get(selectedItem) ?? []}
-                    
+
                     getOptionLabel={(option) => toDisplay(option)}
-                    renderOption={(props, option, {selected}) => (
+                    renderOption={(props, option, { selected }) => (
                         <li {...props}>
                             <Checkbox
                                 {...searchCheckboxProps}
                                 icon={icon}
                                 checkedIcon={checkedIcon}
-                                style={{marginRight: 8}}
+                                style={{ marginRight: 8 }}
                                 checked={selected}
                             />
                             {toDisplay(option)}
                         </li>
                     )}
                     renderInput={(params) => (
-                        <TextField {...params} placeholder={placeholder} sx={{paddingY: 0}}/>
+                        <TextField {...params} placeholder={placeholder} sx={{ paddingY: 0 }} />
                     )}
                 />
             </FormControl>
